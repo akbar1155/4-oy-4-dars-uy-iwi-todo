12d6f92b6953cd8d7fde07d7248cde8df801b11d
Akbar <isroilovakbar49@gmail.com>
2023-02-15 15:32:25 +0500

    erfref

--- main.js
+++ main.js
@@ -3,18 +3,27 @@ const elinput1 = document.querySelector(".input1")
 const eltext1 = document.querySelector(".text1")
 const arr = []
 
+
+
+
 elbox1.addEventListener("change", myFunction);
+function myFunction() {
+    eltext1.innerHTML = elinput1.value
+
+}
 
 
-fetch()
-    .then(function myFunction() {
-        eltext1.innerHTML = elinput1.value
 
-    })
-    .then(function my() {
-        arr.push("elinput1.value")
 
-    })
+// fetch()
+//     .then(function myFunction() {
+//         eltext1.innerHTML = elinput1.value
+
+//     })
+//     .then(function my() {
+//         arr.push("elinput1.value")
+
+//     })
 
 
 
